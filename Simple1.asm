1a1cf32d2db56b430cd27b3c4e2732777d59d3ec
cew216 <cew216@skbl-422-012.ph.ic.ac.uk>
2018-10-08 12:07:11 +0100

    OscilloscopeConnections

--- Simple1.asm
+++ Simple1.asm
@@ -7,13 +7,15 @@
 	org 0x100		    ; Main code starts here at address 0x100
 
 start
+	movlw	0xff
+	movwf	TRISD, ACCESS
 	movlw 	0x0
 	movwf	TRISC, ACCESS	    ; Port C all outputs
 	bra 	test
 loop	movff 	0x10, PORTC
 	incf 	0x10, W, ACCESS
 test	movwf	0x10, ACCESS	    ; Test for end of loop condition
-	movlw 	0xa
+	movf	PORTD, W, ACCESS
 	cpfsgt 	0x10, ACCESS
 	bra 	loop		    ; Not yet finished goto start of loop again
 	goto 	0x0		    ; Re-run program from start
