6beb0ddd9090475ecd9eb6cf89b5750cab547659
cew216 <cew216@skbl-422-012.ph.ic.ac.uk>
2018-10-12 12:10:37 +0100

    ProtoBoard Experiments

--- Simple1.asm
+++ Simple1.asm
@@ -6,24 +6,14 @@
 	
 	org 0x100		    ; Main code starts here at address 0x100
 
-start
-	movlw	0x01
-	movwf	0x04, ACCESS
-	movlw	0xFE		    ;literal 254 onto W
-	movwf	0x03, ACCESS	    ;store 254 in address
-	movlw	0x0		    
-	movwf	0x02, ACCESS	    ;store 0 in address
-	movwf	TRISC, ACCESS	    ;set Port C to outputs
-	lfsr	FSR0, 0x110	    ;load FSR0 with address (12-bit)
-loop	movf	0x02, W, ACCESS	    ;move literal in address to W
-	movwf	POSTINC0	    ;move W into FRS0 address, increase address 
-	incf	0x02, f, ACCESS	    ;increase literal at address 
-	movf	0x03, W		    ;move 254 into W
-	cpfsgt	0x02, ACCESS	    ;compare 254 to 0x02 value
-	bra	loop		    ;will branch if 0x02 < 254
-read	lfsr	FRS1, 0x20E	    ;load address into FSR1
-	movf	POSTDEC1, W, ACCESS ;move FSR1 onto W and decrease FRS1 address
-	movwf	PORTC		    ;move W onto Port C (LEDs)
-	cpfslt	0x04, ACCESS	    ;compare W to 1 (literal stored at address)
-	bra	read
+start	movlw	0x0
+	movwf	TRISD, ACCESS
+	movwf	TRISE, ACCESS	    ;set E to output
+	movlw	0xff
+	movwf	TRISC, ACCESS	    ;set C to input
+	movlw	0x63
+	movwf	0x01, ACCESS	    ;store literal 99 decimal at address
+	movff	0x01, PORTE	    ;move literal at address to PORTE
+input	movf	PORTC, W, ACCESS    ;move PORTC's input to W
+	movwf	PORTD, ACCESS	    ;move W to PORTD
 	end
