18ee23aa4a97a8d5adfdfa9d54d47d6958479fee
cew216 <cew216@skbl-422-012.ph.ic.ac.uk>
2018-10-15 10:33:12 +0100

    Counter and clocks

--- Simple1.asm
+++ Simple1.asm
@@ -6,14 +6,21 @@
 	
 	org 0x100		    ; Main code starts here at address 0x100
 
-start	movlw	0x0
+start	movlw	0xff
+	movwf	0x02, ACCESS
+	movwf	TRISC, ACCESS	    ;set C to input
+restart	movlw	0x0
 	movwf	TRISD, ACCESS
 	movwf	TRISE, ACCESS	    ;set E to output
-	movlw	0xff
-	movwf	TRISC, ACCESS	    ;set C to input
-	movlw	0x63
 	movwf	0x01, ACCESS	    ;store literal 99 decimal at address
-	movff	0x01, PORTE	    ;move literal at address to PORTE
-input	movf	PORTC, W, ACCESS    ;move PORTC's input to W
-	movwf	PORTD, ACCESS	    ;move W to PORTD
-	end
+	movff	0x01, PORTE
+counter	incf	0x01, f, ACCESS
+	movff	0x01, PORTE
+	movf	0x01, W, ACCESS
+	movf	PORTC, W, ACCESS
+	movwf	PORTD, ACCESS
+	cpfsgt	0x02, ACCESS
+	bra	counter
+	bra	counter
+	
+	end 
