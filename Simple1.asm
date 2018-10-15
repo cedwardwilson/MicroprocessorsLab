e271624874531229d001ae982544b6a0e0f8d072
cew216 <cew216@skbl-422-012.ph.ic.ac.uk>
2018-10-15 17:13:30 +0100

    2 byte storage 1.0

--- Simple1.asm
+++ Simple1.asm
@@ -6,21 +6,43 @@
 	
 	org 0x100		    ; Main code starts here at address 0x100
 
-start	movlw	0xff
-	movwf	0x02, ACCESS
-	movwf	TRISC, ACCESS	    ;set C to input
-restart	movlw	0x0
-	movwf	TRISD, ACCESS
-	movwf	TRISE, ACCESS	    ;set E to output
-	movwf	0x01, ACCESS	    ;store literal 99 decimal at address
-	movff	0x01, PORTE
-counter	incf	0x01, f, ACCESS
-	movff	0x01, PORTE
-	movf	0x01, W, ACCESS
-	movf	PORTC, W, ACCESS
-	movwf	PORTD, ACCESS
-	cpfsgt	0x02, ACCESS
-	bra	counter
-	bra	counter
-	
-	end 
+start	movlw	0x51		    ;Define datum for flipflop A
+	movwf	0x01		    ;Address datum for A  
+	movlw	0xaa		    ;Define datum for flipflop B
+	movwf	0x02		    ;Address datum for B
+	clrf	TRISD		    ;Setting PORTD to output
+	clrf	TRISE		    ;Setting PORTE to output
+	clrf	TRISC
+	movlw	0x40		    ;Sets OE* to high for A which turns OE off
+	movwf	PORTD
+	bra	write
+
+delay	nop			    ;delays the CLK whilst it's high
+	nop
+	return
+
+EInput	bsf	PADCFG1, REPU, BANKED	;sets pull up resistors on PORTE
+	movlb	0x0		    ;resets BSR to 0
+	setf	TRISE		    ;sets PORTE to input
+	return
+		
+write	movff	0x01, PORTE	    ;give PORTE a value
+	movlw	0x41		    ;sets OE* and CLK to high 
+	movwf	PORTD
+	call	delay		    
+	movlw	0x40		    ;sets OE* to high and turns off CLK
+	movwf	PORTD
+	call	EInput
+	movlw	0x0		    ;turns OE* off
+	movwf	PORTD		    ;moves data on A to PORTE
+	bra	read
+
+read	movf	PORTE, W	    ;move PORTE data to W
+	movwf	PORTC
+	movlw	0x40		    ;sets OE* to high
+	movwf	PORTD
+	call	delay
+		                          
+stop	
+						    	bra	stop
+	end
